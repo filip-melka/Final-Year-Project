c381edb47ab89ac8fa83a2b5238154f223544aaf
Filip Melka <melka.filip@proton.me>
2026-02-09 12:55:54 +0100

    feat: implement Home page

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,3 +1,28 @@
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { Sparkles } from "lucide-react"
+
 export default function Home() {
-  return <div>Polydoc</div>
+  return (
+    <>
+      <main className="min-h-[calc(80vh-10rem)] flex items-center justify-center">
+        <div className="max-w-xl text-center">
+          <Badge variant="outline">
+            <Sparkles />
+            <span>AI-Powered</span>
+          </Badge>
+          <p className="text-5xl font-semibold mt-4 mb-8">Welcome to Polydoc</p>
+          <p className="opacity-70">
+            A smart translation system that uses a vector database to reduce
+            costs and improve translation consistency.
+          </p>
+          <div className="flex mt-28 gap-4 items-center justify-center">
+            {/* Select file btn */}
+            <Button variant="outline">Learn More</Button>
+          </div>
+        </div>
+      </main>
+      {/* File dialog */}
+    </>
+  )
 }

1a3ab507e34732fc5dd7788359bb067ad9b44f10
Filip Melka <melka.filip@proton.me>
2026-02-11 17:28:11 +0100

    feat: add environment variables for AWS credentials in Terraform workflow

--- backend/terraform/variables.tf
+++ backend/terraform/variables.tf
@@ -3,11 +3,13 @@ variable "bucket_name" {
 }
 
 variable "open_api_key" {
-  default = "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"
+  type      = string
+  sensitive = true
 }
 
 variable "chroma_api_key" {
-  default = "ck-HSqPWJe85KSur1xzPqC1RRkf3E2GqCGMxRdXnxmGucS9"
+  type      = string
+  sensitive = true
 }
 
 variable "lambda_image_uri" {
